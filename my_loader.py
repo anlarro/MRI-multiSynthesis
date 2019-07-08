c3397ef7e54758f06e89faef599df2db1b4c02ee
anlarro <anlarro@gmail.com>
2019-07-08 15:25:17 +0200

    Add padding in get()

--- my_loader.py
+++ my_loader.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-import nibabel as nib
+
 import SimpleITK as sitk
 
 class Data(object):
@@ -20,10 +20,9 @@ class Data(object):
         for mod_name in self.modalities:
             print('Loading ' + mod_name)
             folder=os.path.join(self.folder,mod_name)
-
             # X = [nib.load(os.path.join(folder,f)).get_data() for f in sorted(os.listdir(folder))]
             # X = [np.swapaxes(np.swapaxes(d, 1, 2), 0, 1) for d in X]
-            X_sitk = [sitk.ReadImage(os.path.join(folder, f)) for f in sorted(os.listdir(folder))]
+            X_sitk = [sitk.ReadImage(os.path.join(folder, f)) for f in sorted(os.listdir(folder)) if '_reg' in f] #We load only the co-registered vols identified with _reg
             X = [sitk.GetArrayFromImage(vol_sitk) for vol_sitk in X_sitk]
             print('Loaded %d vols' % len(X))
             X = [X[i].astype('float32') for i in range(len(X))]
@@ -32,9 +31,23 @@ class Data(object):
         self.num_vols = len(X)
         self.patient_names = [f.split('-')[0] for f in sorted(os.listdir(folder))]
 
-    def get(self, modality, ids):
-        data_ids = [self.vols[modality][i] for i in ids]
+    def padVolume(self,volume, padding, default_pixel_value=0):
+        padded = sitk.ConstantPad(volume, [4, 4, 0], [2, 2, 0])
+        return padded
 
+    def get(self, modality, ids):
+        shapes = [np.mean(np.shape(self.vols[modality][i])[1:3]) for i in ids]
+        dim = int(4 * np.ceil(np.max(shapes) / 4)) #make all 2D dimensions divisble by 4
+        data_ids = []
+        for i in ids:
+            shape=np.shape(self.vols[modality][i])
+            data_ids.append(np.pad(
+                self.vols[modality][i],
+                ((0,0), (int(np.floor(abs(dim-shape[1])/2)),
+                         int(np.ceil(abs(dim-shape[1])/2))),
+                 (int(np.floor(abs(dim-shape[2])/2)),
+                  int(np.ceil(abs(dim-shape[2])/2)))),
+                 mode = 'constant'))
         data_ids_ar = np.concatenate(data_ids)
         if len(data_ids_ar.shape) < 4:
             data_ids_ar = np.expand_dims(data_ids_ar, axis=1)
