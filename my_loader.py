7207d578bb528c5b95b7e0bb9349de51480d8a76
anlarro <anlarro@gmail.com>
2019-08-05 15:28:52 +0200

    need to fix concatenations in generator

--- my_loader.py
+++ my_loader.py
@@ -20,60 +20,66 @@ class Data(object):
             train_files = [f for f in sorted(os.listdir(folder_mod)) if '_reg' in f]
             self.total_vols = len(train_files)
 
-    def generate_batches(self, modalities, num_emb = None, batch_size = 16, mode='train'):
+    def generate_batches(self, input_modalities, output_modalities = None, num_emb = None, batch_size = 16, mode='train'):
         #yields batches of data, to use with fit_generator
         folder=self.folders[mode]
         vol_num = 0
-        X = readVolume(folder, vol_num, modalities)
+        X = self.readCase(folder, vol_num, input_modalities)
+        Y = self.readCase(folder, vol_num, output_modalities)
         last_vol = X
         vol_num+=1
         while True: #loop indefinitely (generator)
-
             if X[0].shape[0] <= last_vol[0].shape[0]:
-                X2 = readVolume(folder, vol_num, modalities)
+                X2 = self.readCase(folder, vol_num, input_modalities)
+                Y2 = self.readCase(folder, vol_num, output_modalities)
                 X = [np.vstack((X[i],X2[i])) for i in range(len(X))] #read the next volume, we always keep >1 volume_size in X
+                Y = [np.vstack((Y[i], Y2[i])) for i in range(len(Y))]  # read the next volume, we always keep >1 volume_size in X
                 last_vol = X2
                 vol_num += 1
                 if vol_num == self.total_vols and mode != 'test': # we keep feeding the generator from the fist volume
                     vol_num = 0
-                    X = readVolume(folder, vol_num, modalities)
+                    X = self.readCase(folder, vol_num, input_modalities)
+                    Y = self.readCase(folder, vol_num, output_modalities)
                     last_vol = X
                     vol_num += 1
 
             # we always pop() batch_size from the beginning of X
             if X[0].shape[0] >= batch_size:
-                batch = [X[i][:batch_size].copy() for i in range(len(X))]
+                batch_in = [X[i][:batch_size].copy() for i in range(len(X))]
                 X = [np.delete(X[i], np.array(range(batch_size)), 0) for i in range(len(X))]
+
+                batch_out = [Y[i][:batch_size].copy() for i in range(len(Y))]
+                Y = [np.delete(Y[i], np.array(range(batch_size)), 0) for i in range(len(Y))]
             else:   #this will occur in test mode when we reach the final slices
-                batch = X
+                batch_in = X
+                batch_out = Y
                 X = []
+                Y = []
 
-            if len(batch[0].shape) < 4:
-                batch = [np.expand_dims(batch[i], axis=1) for i in range(len(batch))]
+            if len(batch_in[0].shape) < 4:
+                batch_in = [np.expand_dims(batch_in[i], axis=1) for i in range(len(batch_in))]
+            if len(batch_out[0].shape) < 4:
+                batch_out = [np.expand_dims(batch_out[i], axis=1) for i in range(len(batch_out))]
 
-            if num_emb is not None:
-                # there's 1 output per embedding plus 1 output for the total variance embedding
-                batch = [batch[m] for m in range(len(batch)) for i in range(num_emb)]
-#
-#             train_shape = (train_out[0].shape[0], 1, train_out[0].shape[2], train_out[0].shape[3])
-#
-#             if len(input_modalities) > 1:
-#                 train_out += [np.zeros(shape=train_shape) for i in range(2)]
-# ####################
-            yield(batch)
+            # there's 1 output per embedding plus 1 output for the total variance embedding
+            batch_out = [batch_out[m] for m in range(len(batch_out)) for i in range(num_emb)]
+            batch_shape = (batch_out[0].shape[0], 1, batch_out[0].shape[2], batch_out[0].shape[3])
+            if len(input_modalities) > 1:
+                batch_out += [np.zeros(shape=batch_shape) for i in range(2)]
+            yield(batch_in, batch_out)
 
-    def readVolume(self, folder, vol_num, modalities):
+    def readCase(self, folder, vol_num, modalities):
         array_volume = []
         for mod_name in modalities:
             folder_mod = os.path.join(folder, mod_name)
             files = [f for f in sorted(os.listdir(folder_mod)) if '_reg' in f]  # We load only the co-registered vols identified with _reg
             f = files[vol_num]
             volume = sitk.ReadImage(os.path.join(folder_mod, f))
-            target_size = [int(32 * np.ceil((volume.GetSize()[i] + 32) / 32)) for i in
-                           range(2)]  # make target 2D size to be divisible by 32
+            target_size = [int(64 * np.ceil((volume.GetSize()[i] + 64) / 64)) for i in range(2)]  # make target 2D size to be divisible by 40, to ensure that all slices are of the same size
             volume_padded = self.padVolume(volume,
                                            target_size)  # With padding we ensure that all slices across volumes are the same
             array_volume.append(sitk.GetArrayFromImage(volume_padded).astype('float32'))
+        self.vol_shape = target_size[::-1]
         return array_volume
 
     def padVolume(self,volume, size, default_pixel_value=0):
@@ -85,25 +91,13 @@ class Data(object):
                                   default_pixel_value)
         return padded
 
-    def load(self):    #loads all volumes, to use with model.fit()
-        for mod_name in self.modalities:
-            print('Loading ' + mod_name)
-            folder=os.path.join(self.folder,mod_name)
-            X_sitk = [sitk.ReadImage(os.path.join(folder, f)) for f in sorted(os.listdir(folder)) if '_reg' in f] #We load only the co-registered vols identified with _reg
-            target_size = [int(32 * np.ceil((X_sitk[0].GetSize()[i]+32) / 32)) for i in range(2)] #make target 2D size to be divisible by 32, to ensure that all slices are of the same size
-            X_sitk_padded = [self.padVolume(X,target_size) for X in X_sitk]
-            X = [sitk.GetArrayFromImage(vol_sitk) for vol_sitk in X_sitk_padded]
-            print('Loaded %d vols' % len(X))
-            X = [X[i].astype('float32') for i in range(len(X))]
-            self.vols[mod_name] = X
-            self.vols_sitk[mod_name] = X_sitk   #we keep the original sitk volume without padding
-        self.vol_shape = target_size[::-1]
-        self.num_vols = len(X)
-
-    def get(self, modality, ids):  #get volumes, to use after load, to use with model.fit()
-        data_ids = [self.vols[modality][i] for i in ids]
-
-        data_ids_ar = np.concatenate(data_ids)
-        if len(data_ids_ar.shape) < 4:
-            data_ids_ar = np.expand_dims(data_ids_ar, axis=1)
-        return data_ids_ar
+    def countImages(self, input_modalities, mode = 'train'):    #loads all volumes, to use with model.fit()
+        mod_name = input_modalities[0]
+        folder = self.folders[mode]
+        folder_mod = os.path.join(folder, mod_name)
+        files = [f for f in sorted(os.listdir(folder_mod)) if '_reg' in f]
+        num_images = 0
+        for f in files:
+            volume = sitk.ReadImage(os.path.join(folder_mod, f))
+            num_images+=volume.GetSize()[2]
+        return num_images
--- my_train.py
+++ my_train.py
@@ -1,4 +1,3 @@
-import math
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -9,13 +8,9 @@ from keras.callbacks import EarlyStopping
 
 input_modalities = ['T1','T2','T2-FLAIR']
 output_modalities = ['T1','T2','T2-FLAIR']
-folder = '/mnt/D8D413E4D413C422/I3M/Imagenes/Oasis/data-tiny'
-
-if input_modalities == output_modalities:
-    data = Data(folder, input_modalities)
-else:
-    data = Data(folder, input_modalities + output_modalities)
-data.load()
+folder = '/mnt/D8D413E4D413C422/I3M/Imagenes/Oasis/data-reduced'
+data = Data(folder+'/Training',folder+'/Validation')
+dummy_volume = data.readCase(folder+'/Training', 0, input_modalities) #read one volume to obtain self.vol_shape
 
 #build model
 weights = {m:1.0 for m in output_modalities}
@@ -23,53 +18,30 @@ weights['concat']=1.0
 m = Multimodel(input_modalities, output_modalities, weights, 16, 1, True, 'max', True, True, data.vol_shape)
 m.build()
 
-#Prepare data
-trainFrac = math.floor(data.num_vols*0.7)
-valFrac = math.floor(data.num_vols*0.1)
+# Training params
+NUM_EPOCHS = 1
+BATCH_SIZE = 8
 
-ids_train = range(trainFrac)
-ids_val = range(trainFrac,trainFrac+valFrac)
-
-train_in = [data.get(mod, ids_train) for mod in input_modalities]
-valid_in = [data.get(mod, ids_val) for mod in input_modalities]
-
-# there's 1 output per embedding plus 1 output for the total variance embedding
-train_out = [data.get(mod, ids_train) for mod in output_modalities for i in range(m.num_emb)]
-valid_out = [data.get(mod, ids_val) for mod in output_modalities for i in range(m.num_emb)]
-
-train_shape = (train_out[0].shape[0], 1, train_out[0].shape[2], train_out[0].shape[3])
-valid_shape = (valid_out[0].shape[0], 1, valid_out[0].shape[2], valid_out[0].shape[3])
-if len(input_modalities) > 1:
-    train_out += [np.zeros(shape=train_shape) for i in range(2)]
-    valid_out += [np.zeros(shape=valid_shape) for i in range(2)]
-
-#train
+num_train_images = data.countImages(input_modalities, mode = 'train')
+num_val_images = data.countImages(input_modalities, mode = 'valid')
+# Configure early stopping
 es = EarlyStopping(monitor='val_loss', min_delta=0.01, mode='min', patience=10)
 
-# print('Fitting model...')
-# m.model.fit(train_in, train_out, validation_data=(valid_in, valid_out), nb_epoch=1, batch_size=16,callbacks=[es])
-# m.model.save_weights('my_weights.h5')
+# Initialize both the training and testing image generators
+trainGen = data.generate_batches(input_modalities, output_modalities, m.num_emb, batch_size = BATCH_SIZE, mode = 'train')
 
-#Train by loading batches
-NUM_EPOCHS = 1
-BATCH_SIZE = 16
-
-# initialize both the training and testing image generators
-trainGen_in = data.generate_batches(input_modalities, batch_size = BATCH_SIZE, mode = 'train')
-trainGen_out = data.generate_batches(output_modalities, m.num_emb, batch_size = BATCH_SIZE, mode = 'train')
-
-valGen_in = data.generate_batches(input_modalities, batch_size = BATCH_SIZE, mode = 'valid')
-valGen_out = data.generate_batches(output_modalities, m.num_emb, batch_size = BATCH_SIZE, mode = 'valid')
+valGen = data.generate_batches(input_modalities, output_modalities, m.num_emb, batch_size = BATCH_SIZE, mode = 'valid')
 
 # train the network
 print("Fitting model with generator...")
 H = m.model.fit_generator(
 	trainGen,
-	steps_per_epoch=NUM_TRAIN_IMAGES // BATCH_SIZE,
+	samples_per_epoch=num_train_images,
 	validation_data=valGen,
-	validation_steps=NUM_VAL_IMAGES // BATCH_SIZE,
-	epochs=NUM_EPOCHS,
-    callbacks = [es])
+	nb_val_samples=num_val_images,
+	nb_epoch=NUM_EPOCHS,
+    callbacks = [es],
+    verbose = 2)
 m.model.save_weights('my_weights.h5')
 
 # plot the training loss and accuracy
