6ff81a53ff83b01a99edeeb8c10f34e04084cd54
anlarro <anlarro@gmail.com>
2019-08-06 16:19:08 +0200

    Include generator for train and test

--- my_loader.py
+++ my_loader.py
@@ -25,22 +25,27 @@ class Data(object):
         #yields batches of data, to use with fit_generator
         folder=self.folders[mode]
         vol_num = 0
-        X = self.readCase(folder, vol_num, input_modalities)
-        Y = self.readCase(folder, vol_num, output_modalities)
+        X,_ = self.readCase(folder, vol_num, input_modalities)
+        if output_modalities is not None:
+            Y,_ = self.readCase(folder, vol_num, output_modalities)
         last_vol = X
         vol_num+=1
+        batch_out = []
         while True: #loop indefinitely (generator)
             if X[0].shape[0] <= last_vol[0].shape[0]:
-                X2 = self.readCase(folder, vol_num, input_modalities)
-                Y2 = self.readCase(folder, vol_num, output_modalities)
+                X2,_ = self.readCase(folder, vol_num, input_modalities)
                 X = [np.vstack((X[i],X2[i])) for i in range(len(X))] #read the next volume, we always keep >1 volume_size in X
-                Y = [np.vstack((Y[i], Y2[i])) for i in range(len(Y))]  # read the next volume, we always keep >1 volume_size in X
+
+                if output_modalities is not None:
+                    Y2,_ = self.readCase(folder, vol_num, output_modalities)
+                    Y = [np.vstack((Y[i], Y2[i])) for i in range(len(Y))]  # read the next volume, we always keep >1 volume_size in X
                 last_vol = X2
                 vol_num += 1
                 if vol_num == self.total_vols and mode != 'test': # we keep feeding the generator from the fist volume
                     vol_num = 0
-                    X = self.readCase(folder, vol_num, input_modalities)
-                    Y = self.readCase(folder, vol_num, output_modalities)
+                    X,_ = self.readCase(folder, vol_num, input_modalities)
+                    if output_modalities is not None:
+                        Y,_ = self.readCase(folder, vol_num, output_modalities)
                     last_vol = X
                     vol_num += 1
 
@@ -49,24 +54,27 @@ class Data(object):
                 batch_in = [X[i][:batch_size].copy() for i in range(len(X))]
                 X = [np.delete(X[i], np.array(range(batch_size)), 0) for i in range(len(X))]
 
-                batch_out = [Y[i][:batch_size].copy() for i in range(len(Y))]
-                Y = [np.delete(Y[i], np.array(range(batch_size)), 0) for i in range(len(Y))]
+                if output_modalities is not None:
+                    batch_out = [Y[i][:batch_size].copy() for i in range(len(Y))]
+                    Y = [np.delete(Y[i], np.array(range(batch_size)), 0) for i in range(len(Y))]
             else:   #this will occur in test mode when we reach the final slices
                 batch_in = X
-                batch_out = Y
                 X = []
-                Y = []
+                if output_modalities is not None:
+                    batch_out = Y
+                    Y = []
 
             if len(batch_in[0].shape) < 4:
                 batch_in = [np.expand_dims(batch_in[i], axis=1) for i in range(len(batch_in))]
-            if len(batch_out[0].shape) < 4:
-                batch_out = [np.expand_dims(batch_out[i], axis=1) for i in range(len(batch_out))]
 
-            # there's 1 output per embedding plus 1 output for the total variance embedding
-            batch_out = [batch_out[m] for m in range(len(batch_out)) for i in range(num_emb)]
-            batch_shape = (batch_out[0].shape[0], 1, batch_out[0].shape[2], batch_out[0].shape[3])
-            if len(input_modalities) > 1:
-                batch_out += [np.zeros(shape=batch_shape) for i in range(2)]
+            if output_modalities is not None:
+                if len(batch_out[0].shape) < 4:
+                    batch_out = [np.expand_dims(batch_out[i], axis=1) for i in range(len(batch_out))]
+                    # there's 1 output per embedding plus 1 output for the total variance embedding
+                    batch_out = [batch_out[m] for m in range(len(batch_out)) for i in range(num_emb)]
+                    batch_shape = (batch_out[0].shape[0], 1, batch_out[0].shape[2], batch_out[0].shape[3])
+                    if len(input_modalities) > 1:
+                        batch_out += [np.zeros(shape=batch_shape) for i in range(2)]
             yield(batch_in, batch_out)
 
     def readCase(self, folder, vol_num, modalities):
@@ -78,7 +86,7 @@ class Data(object):
             volume = sitk.ReadImage(os.path.join(folder_mod, f))
             volume_padded = self.padVolume(volume, self.target_size)  # With padding we ensure that all slices across volumes are the same
             array_volume.append(sitk.GetArrayFromImage(volume_padded).astype('float32'))
-        return array_volume
+        return array_volume, volume
 
     def padVolume(self,volume, size, default_pixel_value=0):
         shape = volume.GetSize()
--- my_test.py
+++ my_test.py
@@ -10,15 +10,15 @@ import numpy as np
 folder = '/mnt/D8D413E4D413C422/I3M/Imagenes/Oasis/data-reduced'
 
 #The model was trained with all possible inputs and outputs, so we have one sigle model.
-# At testing time the required output is not inclued as input. For example: output: T2-FLAIR - inputs: T1-T2
+# At testing time the required output is not included as input. For example: output: T2-FLAIR - inputs: T1-T2
 training_inputs = ['T1','T2','T2-FLAIR']
 training_outputs = ['T1','T2','T2-FLAIR']
 
-if training_inputs == training_outputs:
-    data = Data(folder, training_inputs)
-else:
-    data = Data(folder, training_inputs + training_outputs)
-data.load()
+data = Data(train_folder = folder+'/Training',test_folder = folder+'/Testing')
+print("Counting total number of images...")
+# we need to count training images again in order to have the correct data.vol_shape
+num_train_images, _ = data.countImages(training_inputs, mode = 'train')
+num_test_images, test_vols = data.countImages(training_inputs, mode = 'test')
 
 #build model
 weights = {m:1.0 for m in training_outputs}
@@ -35,11 +35,6 @@ try:
 except OSError:
     print ("Output folder already exist")
 
-trainFrac = math.floor(data.num_vols*0.7)
-valFrac = math.floor(data.num_vols*0.1)
-testFrac = math.floor(data.num_vols*0.2)
-ids_test = range(trainFrac+valFrac,trainFrac+valFrac+testFrac)
-
 inputs = []
 for n in range(1,len(training_outputs)):
     inputs.extend(list(itertools.combinations(training_inputs, n)))
@@ -48,20 +43,22 @@ outputs = []
 for i in inputs:
     outputs.append(tuple(set(i).symmetric_difference(training_inputs)))
 
-for vol_num in ids_test:
+BATCH_SIZE = 1
+for vol_num in range(test_vols):
     print('testing model on volume ' + str(vol_num) + '...')
+    _, current_sitkVol = data.readCase(data.folders['test'], vol_num, [training_inputs[0]])
+    slices_per_volume = current_sitkVol.GetSize()[2]
     for i in range(len(inputs)):
         input_modalities = list(inputs[i])
         output_modalities = list(outputs[i])
         for o in output_modalities:
-            X = [data.get(mod, [vol_num]) for mod in input_modalities]
+            testGen = data.generate_batches(input_modalities, batch_size=BATCH_SIZE, mode='test')
             partial_model = m.get_partial_model(input_modalities, o)
-            Z = partial_model.predict(X)
-                # there's 1 output per embedding plus 1 output for the total variance embedding (that's why we iterate as follows)
+            Z = partial_model.predict_generator(testGen, val_samples = slices_per_volume)
 
             index = len(input_modalities)
             padded_size = np.squeeze(Z[index]).shape
-            no_padded_size = data.vols_sitk[input_modalities[0]][vol_num].GetSize()[::-1]
+            no_padded_size = current_sitkVol.GetSize()[::-1]
             Z_sitk = sitk.GetImageFromArray(np.squeeze(Z[index])[:,
                                             int(np.floor((padded_size[1] - no_padded_size[1]) / 2)):
                                             no_padded_size[1] + int(
@@ -70,7 +67,7 @@ for vol_num in ids_test:
                                             no_padded_size[2] + int(
                                             np.floor((padded_size[2] - no_padded_size[2]) / 2))
                                             ])
-            Z_sitk.CopyInformation(data.vols_sitk[input_modalities[0]][vol_num])
+            Z_sitk.CopyInformation(current_sitkVol)
             sitk.WriteImage(Z_sitk, os.path.join(output_folder, data.test_names[vol_num] +
                                                 '_in_' + "--".join(input_modalities) + '_out_' + o + '.nii.gz'))
 
